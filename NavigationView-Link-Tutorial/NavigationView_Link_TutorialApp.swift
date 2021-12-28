807bd31d05b2b92d5421e05937f798c1041c073c
James Boyer <jamesboyer92@user.noreply.github.com>
2021-12-28 16:45:11 -0500

    Initial Commit

---
+++ NavigationView-Link-Tutorial/NavigationView_Link_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationView_Link_TutorialApp.swift
+//  NavigationView-Link-Tutorial
+//
+//  Created by James Boyer on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationView_Link_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
